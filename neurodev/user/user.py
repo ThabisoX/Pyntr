31aad1de6e1541bb30dcd514d62ecc3aa7d27501
Thabiso <thabisoxulu4@gmail.com>
2025-04-25 11:20:49 +0200

    Update user.py

--- neurodev/user/user.py
+++ neurodev/user/user.py
@@ -3,11 +3,33 @@ def register_user():
     email = input("Email: ")
     username = input('Username: ')
     password = input('Password: ')
+    
+    user_data = {
+    "username": username,
+    "email": email,
+    "password": password, 
+    "username": username,
+    "plan": "free",  # default plan
+    "interests": []  
+}
+    return user_data
+
 
 
 
 
 def login_user():
     email_log = input('Email: ')
-    password_log = input('Password')
-    return 
+    password_log = input('Password: ')
+    
+    print("Logging in...")  
+
+
+    print("Logging in...")  
+
+    return {
+    "username": "pyntr_user",
+    "email": email_log,
+    "plan": "free",
+    "interests": ["Backend", "DevOps"]
+}
