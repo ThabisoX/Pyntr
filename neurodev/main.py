9feecbd0d6f2c62e4fd246496e4fd721521d88b5
Thabiso <thabisoxulu4@gmail.com>
2025-04-25 04:19:37 +0200

    main.py inputs and menu
    first entry of application user interface logic

---
+++ neurodev/main.py
@@ -0,0 +1,46 @@
+from user import user
+
+
+print("Hello World! Welcome to PYNTR")
+print('A developers playground!')
+print("Would you like to : ")
+print('1. Register         2. Login        3.esc?')
+reg_log = int(input('Enter number: '))
+
+if reg_log == 1:
+    current_user = user.register_user()
+    print('User registered Successfully!\n')
+    main_menu(current_user)
+elif reg_log == 2:
+    current_user = user.login_user()
+    main_menu(current_user)
+elif reg_log == 3:
+    exit()
+else:
+    exit()
+    
+def main_menu(current_user):
+    while True :
+        print(f"\n👋 Welcome {current_user['username']}! What would you like to do?")
+        print("[1]Connect with another dev (Synapse)")
+        print("[2] Post a Pulse")
+        print('[3] Join a Caucus')
+        print("[4] See Notifications")
+        print("[5] Upgrade Plan")
+        print('[6] Logout')
+    
+    
+        option =int(input('Enter Option (1-6): '))
+    
+        if option == 1:
+            print("Feature under construction: Synapse.")
+        elif option == 2:
+            print("Feature under construction: Synapse.")
+        elif option == 3:
+            print("Feature under construction: Synapse.")
+        elif option == 4:
+            print("Feature under construction: Synapse.")
+        elif option == 5:
+            print("Feature under construction: Synapse.")
+        elif option == 6:
+            break 
---
+++ neurodev/user/user.py
@@ -0,0 +1,13 @@
+def register_user():
+    name = input("Full Name: ")
+    email = input("Email: ")
+    username = input('Username: ')
+    password = input('Password: ')
+
+
+
+
+def login_user():
+    email_log = input('Email: ')
+    password_log = input('Password')
+    return 
